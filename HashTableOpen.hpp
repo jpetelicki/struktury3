345bd6fac7bf854b0a0a5e1c848c31f2547436f9
Jakub Petelicki <kuba.petelicki@gmail.com>
2026-06-12 12:47:33 +0200

    Dodaj pliki projektów.

---
+++ HashTableOpen.hpp
@@ -0,0 +1,31 @@
+﻿#pragma once
+
+struct HashNode {
+    int key;
+    int value;
+    HashNode* next; //wskaźnik na kolejny element w przypadku kolizji
+
+    HashNode(int k, int v) : key(k), value(v), next(nullptr) {}
+};
+
+class HashTableList {
+private:
+
+    HashNode** table; //dynamiczna tablica przechowująca Hash Nody
+
+    int capacity;
+    int size;
+
+    int hashFunction(int key) const;
+
+    void rehash();
+
+public:
+    HashTableList(int initialCapacity);
+    ~HashTableList();
+
+    void insert(int key, int value);
+    void remove(int key);
+    bool search(int key, int& outValue) const;
+    void print() const;
+};
